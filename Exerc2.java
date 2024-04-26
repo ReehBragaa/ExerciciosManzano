b4cbb618711f2de3a339a895555f668a253bfcae
ReehBragaa <159200857+ReehBragaa@users.noreply.github.com>
2024-04-26 16:24:18 -0300

    Exerc2.java

---
+++ Exerc2.java
@@ -0,0 +1,17 @@
+package manzano;
+
+public class Exerc2 {
+
+	public static void main(String[] args) {
+
+		int soma = 0;
+
+		for (int i = 1; i <= 100; i++) {
+			soma += i;
+		}
+
+		System.out.println("Total da soma dos cem primeiros números inteiros: " + soma);
+
+	}
+
+}
