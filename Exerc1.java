68f0252a632623457f35e90b679320be118c592b
ReehBragaa <159200857+ReehBragaa@users.noreply.github.com>
2024-04-26 16:23:40 -0300

    Exerc1.java

---
+++ Exerc1.java
@@ -0,0 +1,24 @@
+package manzano;
+
+import java.util.Scanner;
+
+public class Exerc1 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite um número para a tabuada: ");
+		int numero = scanner.nextInt();
+
+		System.out.println("Tabuada de multiplicar para o número " + numero + ":");
+		for (int i = 1; i <= 10; i++) {
+			int resultado = numero * i;
+			System.out.println(numero + " x " + i + " = " + resultado);
+		}
+
+		scanner.close();
+
+	}
+
+}
